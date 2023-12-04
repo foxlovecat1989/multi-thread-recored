3919b738a58d418cd5012632464cd06a37102bba
foxlovecatii <foxlovecat1989@gmail.com>
2023-12-05 01:44:49 +0800

    section2 - thread coordination - interrupted

---
+++ src/main/java/section2threadcoordinationinterrupted/example1/NormalInterruptExample.java
@@ -0,0 +1,22 @@
+package section2threadcoordinationinterrupted.example1;
+
+public class NormalInterruptExample {
+    public static void main(String [] args) {
+        Thread thread = new Thread(new BlockingTask());
+        thread.start();
+        thread.interrupt();
+    }
+
+    private static class BlockingTask implements Runnable {
+
+        @Override
+        public void run() {
+            //do things
+            try {
+                Thread.sleep(500000);
+            } catch (InterruptedException e) {
+                System.out.println("Existing blocking thread");
+            }
+        }
+    }
+}
---
+++ src/main/java/section2threadcoordinationinterrupted/quiz1/CorrectInterruptExample.java
@@ -0,0 +1,26 @@
+package section2threadcoordinationinterrupted.quiz1;
+
+public class CorrectInterruptExample {
+    public static void main(String [] args) {
+        Thread thread = new Thread(new BlockingTask());
+        thread.start();
+        thread.interrupt();
+    }
+
+    private static class BlockingTask implements Runnable {
+
+        @Override
+        public void run() {
+            //do things
+           while (true) {
+               try {
+                   Thread.sleep(5);
+               } catch (InterruptedException e) {
+                   System.out.println("Existing blocking thread");
+                   // !!! import - without return, the thread will not terminate
+                   return;
+               }
+           }
+        }
+    }
+}
