6ad0b4fed4745cf4ce28c77d5eafdf8dcde457c8
foxlovecatii <foxlovecat1989@gmail.com>
2023-12-09 02:37:47 +0800

    section2 - thread coordination - join

---
+++ src/main/java/section2threadcoordinationjoin/exec1/ComplexCalculation.java
@@ -0,0 +1,46 @@
+package section2threadcoordinationjoin.exec1;
+
+import java.math.BigInteger;
+
+public class ComplexCalculation {
+    public static void main(String[] args) {
+        BigInteger result = calculateResult(BigInteger.valueOf(1000L), BigInteger.valueOf(1000L), BigInteger.valueOf(1000L), BigInteger.valueOf(1000L));
+        System.out.println(result);
+    }
+
+    public static BigInteger calculateResult(BigInteger base1, BigInteger power1, BigInteger base2, BigInteger power2) {
+        PowerCalculatingThread t1 = new PowerCalculatingThread(base1, power1);
+        PowerCalculatingThread t2 = new PowerCalculatingThread(base2, power2);
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return t1.getResult().add(t2.getResult());
+    }
+
+    private static class PowerCalculatingThread extends Thread {
+        private BigInteger result = BigInteger.ONE;
+        private final BigInteger base;
+        private final BigInteger power;
+
+        public PowerCalculatingThread(BigInteger base, BigInteger power) {
+            this.base = base;
+            this.power = power;
+        }
+
+        @Override
+        public void run() {
+            for(BigInteger i = BigInteger.ZERO; i.compareTo(power) !=0; i = i.add(BigInteger.ONE)) {
+                result = result.multiply(base);
+            }
+        }
+
+        public BigInteger getResult() { return result; }
+    }
+}
